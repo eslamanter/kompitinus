770f2c31727a26f9d609e28f40897ac6587e0b7c
Eslam Abdullah Anter <eslam.anter@outlook.com>
2025-04-30 00:25:55 +0200

    Initial Commit

---
+++ sqlite_db.py
@@ -0,0 +1,21 @@
+import sqlite3
+import os
+import config
+from constants import *
+
+def main_db_conncet():
+    if os.path.exists(config.db_path[UI_MAIN]):
+        conn = sqlite3.connect(config.db_path[UI_MAIN])
+        cursor = conn.cursor()
+        print("Connected to the existing database successfully!")
+
+        cursor.execute("SELECT * FROM employees;")
+        print(cursor.fetchall())
+
+        conn.close()
+    else:
+        print("Error: Database does not exist!")
+
+
+if __name__ == "__main__":
+    main_db_conncet()
