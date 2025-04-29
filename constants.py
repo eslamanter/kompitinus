d2a976d803699c050220536feb87eef51967b496
Eslam Abdullah Anter <eslam.anter@outlook.com>
2025-04-29 23:20:24 +0200

    Initial Commit

---
+++ constants.py
@@ -0,0 +1,95 @@
+# Constants
+
+# App
+APP_NAME = "HYtask"
+
+# Database
+
+    # Employees
+DB_EMPLOYEE_ID = "employee_id"
+DB_FIRST_NAME = "first_name"
+DB_LAST_NAME = "last_name"
+DB_EMAIL = "email"
+DB_PIN = "pin"
+DB_REGISTERED_AT = "registered_at"
+DB_ACTIVE = "active"
+
+    # Tasks
+DB_TASK_ID  = "task_id"
+DB_SENDER_ID = "sender_id"
+DB_RECEIVER_ID = "receiver_id"
+DB_CREATED_AT = "created_at"
+DB_MODIFIED_AT =  "modified_at"
+DB_TITLE = "title"
+DB_BODY = "body"
+DB_REFERENCE = "reference"
+DB_DUE_AT = "due_at"
+DB_STARRED = "starred"
+DB_STATUS = "status"
+DB_EXPECTED_AT = "expected_at"
+DB_REPLY = "reply"
+DB_ARCHIVED = "archived"
+
+# UI Text
+
+    # Menubar
+UI_CONFIG = "Config"
+UI_DATABASE = "Database"
+UI_MAIN = "Main"
+UI_LOCAL = "Local"
+UI_USER = "User"
+UI_EDIT = "Edit"
+UI_LOGOUT = "Logout"
+UI_SYNC_MODE = "Sync"
+UI_MANUAL = "Manual"
+UI_AUTO = "Auto"
+UI_1MIN = "1 min"
+UI_5MIN = "5 min"
+UI_10MIN = "10 min"
+UI_15MIN = "15 min"
+UI_30MIN = "30 min"
+UI_60MIN = "60 min"
+UI_NEVER = "Never"
+UI_SYNC_TASK = "Sync"
+UI_SEND_TASK = "Send"
+UI_INFO = "Info"
+UI_ABOUT = "About"
+UI_HELP = "Help"
+
+    # Treeview
+UI_INBOX = "Inbox"
+UI_OUTBOX = "Outbox"
+UI_SELFBOX = "Selfbox"
+UI_STARREDBOX = "Starred"
+UI_ARCHIVEDBOX = "Archive"
+
+    # Task details panel
+UI_TASK_ID = "Task ID"
+UI_CREATED_AT = "Created at"
+UI_MODIFIED_AT = "Modified at"
+UI_SENDER = "Sender"
+UI_RECEIVER = "Receiver"
+UI_TASK = "Task"
+UI_STARRED = "Starred"
+
+UI_REFERENCE = "Reference"
+UI_REFERENCE_OPEN = "Browse Folder"
+UI_REFERENCE_SELECT = "Select Folder"
+UI_REFERENCE_COPY = "Copy Link"
+UI_REFERENCE_PASTE = "Paste Link"
+UI_REFERENCE_DELETE = "Delete Link"
+
+UI_DUE_AT = "Due at"
+UI_EXPECTED_AT = "Expected at"
+UI_DAYS = "Days"
+UI_REPLY = "Reply"
+UI_DONE = "Done"
+UI_ARCHIVED = "Archive"
+UI_UPDATE_TASK = "Update Task"
+
+    # Statusbar
+UI_SELECTED = "Selected"
+UI_COPIED = "Copied"
+UI_PASTED = "Pasted"
+UI_DELETED = "Deleted"
+
